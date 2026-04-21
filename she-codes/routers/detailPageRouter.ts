5c9ab43569568459afd68c12233f912a8d0ef4d1
Samar Asla <samar.asla@student.ap.be>
2026-04-21 16:36:24 +0000

    collection button in menu added

--- she-codes/routers/detailPageRouter.ts
+++ she-codes/routers/detailPageRouter.ts
@@ -6,6 +6,7 @@ export function detailPageRouter() {
 
   router.get("/:mbid", async (req, res) => {
     const mbid = req.params.mbid;
+    const q: string = typeof req.query.q === "string" ? req.query.q : "";
 
     if (!mbid) {
       return res.status(400).render("error", { message: "Artist ID is required" });
@@ -13,7 +14,7 @@ export function detailPageRouter() {
 
     try {
       const data = await getFullArtistData(mbid);
-      res.render("detailPage", { data });
+      res.render("detailPage", { data ,q});
     } catch (err) {
       console.error("Detail page error:", err);
       res.status(500).render("error", { message: "Could not load artist data" });
